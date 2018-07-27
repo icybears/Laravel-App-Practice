8438426c4322ae37972d2aac68e7a7cd0f0e7856
I. Saber <isaber23@outlook.com>
2018-07-27 13:30:48 +0100

    fixed middleware for registration

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -9,6 +9,12 @@ use App\Room;
 
 class RegistrationController extends Controller
 {
+
+  public function __construct ()
+  {
+    $this->middleware('guest');
+  }
+
   public function create () 
   {
     return view('/register');
